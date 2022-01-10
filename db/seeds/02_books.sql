56c6cf0a1c28ad3de02bbd78b8add5efbe2a9b8d
Noya Onggud <yanuo44@gmail.com>
2022-01-10 04:02:56 -0500

    updated db with real book data

--- db/seeds/02_books.sql
+++ db/seeds/02_books.sql
@@ -1,26 +1,137 @@
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (1, 'Heavenly Creatures', 0, 2, 'http://comcast.net/neque/libero/convallis/eget/eleifend/luctus/ultricies.json', 'Aliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.', 1, false, true);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (2, 'Unfinished Song (Song for Marion)', 0, 3, 'http://g.co/consectetuer/adipiscing/elit/proin.json', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+insert into books (id, title, author, image,
+price, rating,review, seller_id,isFeatured, isSold) values
+(
+  '3',
+    'Portable Pets',
+    'Lorella Rizzati',
+    'https://covers.openlibrary.org/b/id/2646149-M.jpg',
+    286,
+    4,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    1,
+    true,
+    false
+),
 
-Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 2, true, true);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (3, 'Burnt Offerings', 0, 1, 'http://walmart.com/arcu/libero/rutrum/ac/lobortis/vel/dapibus.json', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.', 3, true, true);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (4, 'That Darn Cat!', 0, 5, 'http://goo.ne.jp/morbi.png', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 4, true, false);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (5, 'Vamps', 0, 2, 'https://ed.gov/pede/morbi/porttitor/lorem.json', 'Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.
+(
+'1',
+    'Owl babies',
+    'Martin Waddell',
+    'https://covers.openlibrary.org/b/id/6602047-M.jpg',
+    119,
+    2,
+    'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    2,
+    false,
+    false
 
-Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.
+),
 
-Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.', 5, false, true);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (6, 'Something for Everyone', 0, 3, 'https://lycos.com/commodo/vulputate.js', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 6, false, true);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (7, 'In Old Arizona', 0, 5, 'http://kickstarter.com/nisl/duis/ac/nibh.png', 'Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis. Ut at dolor quis odio consequat varius.
+(
+  '5',
+    'Johnny Lions book',
+    'Edith Thacher Hurd',
+    'https://covers.openlibrary.org/b/id/51606-M.jpg',
+    206,
+    1,
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    3,
+    true,
+    true
+),
 
-Integer ac leo. Pellentesque ultrices mattis odio. Donec vitae nisi.', 7, true, false);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (8, 'Rosenstrasse', 0, 1, 'http://joomla.org/pretium/nisl/ut.png', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.
+(
+'10',
+    'Growing frogs',
+    'Vivian French',
+    'https://covers.openlibrary.org/b/id/515082-M.jpg',
+    87,
+    5,
+    'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.',
+    4,
+    true,
+    false
+),
 
-Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.', 8, false, false);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (9, 'Sleepwalk with Me', 0, 5, 'https://hubpages.com/eget/nunc/donec/quis.jsp', 'In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.
+(
+   '6',
+    'Pencil, paper, draw!',
+    'Steve Harpster',
+    'https://covers.openlibrary.org/b/id/1738755-M.jpg',
+    11,
+    2,
+    'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
+    5,
+    false,
+    true
+),
 
-Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.
+(
+ '2',
+    'El canguro tiene Mama?',
+    'Eric Carle',
+    'https://covers.openlibrary.org/b/id/11625175-M.jpg',
+    249,
+    3,
+    'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus.',
+    6,
+    true,
+    false
+)
+,
 
-Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 9, false, false);
-insert into books (id, name, price, rating, image, review, seller_id, isFeatured, isSold) values (10, 'Other Voices, Other Rooms', 0, 3, 'http://wired.com/fusce/lacus/purus.html', 'Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.
+(
+ '4',
+    'Happy Baby',
+    'Roger Priddy',
+    'https://covers.openlibrary.org/b/id/2371344-M.jpg',
+    83,
+    5,
+    'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.',
+    7,
+    false,
+    false
+),
 
-Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 10, false, true);
+(
+'7',
+    'The little wood duck.',
+    'Brian Wildsmith',
+    'https://covers.openlibrary.org/b/id/4600916-M.jpg',
+    90,
+    4,
+    'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
+    8,
+    true,
+    false
+),
+
+
+
+(
+  '9',
+    'Lad, a dog',
+    'Margo Lundell',
+    'https://covers.openlibrary.org/b/id/385802-M.jpg',
+    161,
+    2,
+    'Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.',
+    9,
+    true,
+    false
+),
+
+(
+ '8',
+    'Cowgirl Kate and Cocoa',
+    'Erica Silverman',
+    'https://covers.openlibrary.org/b/id/11024980-M.jpg',
+    216,
+    4,
+    'Nullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris. Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis.',
+    10,
+    true,
+    false
+);
+
+
