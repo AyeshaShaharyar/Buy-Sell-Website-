c12274cb60d73bf48e73d5dc3c1904f14124f7dd
Noya Onggud <yanuo44@gmail.com>
2022-01-12 13:08:26 -0500

    books seed update

--- db/seeds/02_books.sql
+++ db/seeds/02_books.sql
@@ -1,7 +1,7 @@
-insert into books (id, title, image,
+insert into books ( title, image,
 price, rating,review, seller_id,isFeatured, isSold, description, condition, category_id) values
 (
-  '3',
+
     'Portable Pets',
     -- 'Lorella Rizzati',
     'https://covers.openlibrary.org/b/id/2646149-M.jpg',
@@ -17,7 +17,7 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
 ),
 
 (
-'1',
+
     'Owl babies',
     -- 'Martin Waddell',
     'https://covers.openlibrary.org/b/id/6602047-M.jpg',
@@ -33,7 +33,7 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
 ),
 
 (
-  '5',
+
     'Johnny Lions book',
     -- 'Edith Thacher Hurd',
     'https://covers.openlibrary.org/b/id/51606-M.jpg',
@@ -49,7 +49,7 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
 ),
 
 (
-'10',
+
     'Growing frogs',
     -- 'Vivian French',
     'https://covers.openlibrary.org/b/id/515082-M.jpg',
@@ -67,7 +67,7 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
 ),
 
 (
-   '6',
+
     'Pencil, paper, draw!',
     -- 'Steve Harpster',
     'https://covers.openlibrary.org/b/id/1738755-M.jpg',
@@ -84,7 +84,7 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
 ),
 
 (
- '2',
+
     'El canguro tiene Mama?',
     -- 'Eric Carle',
     'https://covers.openlibrary.org/b/id/11625175-M.jpg',
@@ -101,7 +101,6 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
 ,
 
 (
- '4',
     'Happy Baby',
     -- 'Roger Priddy',
     'https://covers.openlibrary.org/b/id/2371344-M.jpg',
@@ -117,7 +116,6 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
 ),
 
 (
-'7',
     'The little wood duck.',
     -- 'Brian Wildsmith',
     'https://covers.openlibrary.org/b/id/4600916-M.jpg',
@@ -135,7 +133,6 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
 
 
 (
-  '9',
     'Lad, a dog',
     -- 'Margo Lundell',
     'https://covers.openlibrary.org/b/id/385802-M.jpg',
@@ -147,11 +144,10 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
     false,
       'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
     'New',
-    15
+    5
 ),
 
 (
- '8',
     'Cowgirl Kate and Cocoa',
     -- 'Erica Silverman',
     'https://covers.openlibrary.org/b/id/11024980-M.jpg',
@@ -163,7 +159,149 @@ price, rating,review, seller_id,isFeatured, isSold, description, condition, cate
     false,
       'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
   'New',
-  15
+  5
 );
+
+
+insert into books ( title, image,
+price, rating,review, seller_id,isFeatured, isSold, description, condition, category_id) values
+
+(
+    'The war of the worlds',
+    -- 'H. G. Wells',
+    'https://covers.openlibrary.org/b/id/431092-M.jpg',
+    286,
+    4,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    1,
+    true,
+    false,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    27),
+
+        (
+    'Nineteen eighty-four',
+    -- 'George Orwell',
+    'https://covers.openlibrary.org/b/id/7898938-M.jpg',
+    119,
+    2,
+    'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    2,
+    false,
+    false,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    27),
+    (
+    'The invisible man',
+    -- 'H. G. Wells',
+    'https://covers.openlibrary.org/b/id/8237011-M.jpg',
+    206,
+    1,
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    3,
+    true,
+    true,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    27),
+    (
+    'The time machine',
+    -- 'H. G. Wells',
+    'https://covers.openlibrary.org/b/id/230683-M.jpg',
+    87,
+    5,
+    'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.',
+    4,
+    true,
+    false,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    27),
+    (
+    'Brave New World',
+    -- 'Aldous Huxley',
+    'https://covers.openlibrary.org/b/id/7894812-M.jpg',
+    11,
+    2,
+    'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
+    5,
+    false,
+    true,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    27);
+
+
+insert into books ( title, image,
+price, rating,review, seller_id,isFeatured, isSold, description, condition, category_id) values
+(    'Empires of the plain',
+    -- 'Lesley Adkins',
+    'https://covers.openlibrary.org/b/id/7972506-M.jpg',
+    286,
+    4,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    1,
+    true,
+    false,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    14
+    ),
+ (
+    'Nuestra especie',
+    -- 'Marvin Harris',
+    'https://covers.openlibrary.org/b/id/9322618-M.jpg',
+    119,
+    2,
+    'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    2,
+    false,
+    false,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    14),
+ (
+    'World prehistory and archaeology',
+    -- 'Michael Chazan',
+    'https://covers.openlibrary.org/b/id/11746826-M.jpg',
+    206,
+    1,
+    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus. Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis. Duis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.',
+    3,
+    true,
+    true,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    14 ) ,
+ (
+    'Ancient lives',
+    -- 'Brian M. Fagan',
+    'https://covers.openlibrary.org/b/id/191804-M.jpg',
+    87,
+    5,
+    'In hac habitasse platea dictumst. Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.',
+    4,
+    true,
+    false,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    14)
+ ,
+(    'World History For Dummies',
+    -- 'Peter Haugen',
+    'https://covers.openlibrary.org/b/id/8400625-M.jpg',
+    11,
+    2,
+    'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.',
+    5,
+    false,
+    true,
+    'Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.',
+    'New',
+    14
+)
+
 
 
