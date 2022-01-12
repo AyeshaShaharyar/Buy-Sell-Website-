e543256cd25c4df2dc2972d9e88101553df9c42d
Noya Onggud <yanuo44@gmail.com>
2022-01-12 12:19:20 -0500

    added science fiction books

--- helpers/BookSeedFetcher/fetch10books_A.js
+++ helpers/BookSeedFetcher/fetch10books_A.js
@@ -3,20 +3,27 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 
 let title, coverMedium,authors;
 let isbnNum;
+
 const tenBooksISBN = [
-  "1564021017",
-  "0060011106",
-  "9780810956339",
-  "9780312490263",
-  "0064442977",
+//first ten is children's books
+  // "1564021017",
+  // "0060011106",
+  // "9780810956339",
+  // "9780312490263",
+  // "0064442977",
 
-  "9781402729768",
-  "0192796860",
-  "9780152053901",
-  "0590929747",
-  "0763603171"
+  // "9781402729768",
+  // "0192796860",
+  // "9780152053901",
+  // "0590929747",
+  // "0763603171"
 
-
+//next five is science fiction
+"0688131379",
+"0060929871",
+"0375761187",
+"0486270718",
+"0452284236"
 ]
 
 let responses=[];
@@ -30,16 +37,17 @@ for (let i=0; i<tenBooksISBN.length; i++)
   .then(json => {
     let book=`ISBN:${tenBooksISBN[i]}`;
     // console.log(book);
-    // console.log(json);
+    // console.log(json[book].subjects);
     // console.log(json[book].title);
     title = json[book].title;
     coverMedium=json[book].cover.medium;
     authors=json[book].authors[0].name;
+    // category=json[book].subjects[0].name;
     // console.log({authors, coverMedium, title});
     let price,rating,review,seller_id,isFeatured,isSold;
     let j=i+1;
     responses.push( `('${j}','${title}', '${authors}', '${coverMedium}')` );
-    console.log(responses); 
+    console.log(responses);
   })
   .catch(err => console.log(err));
 }
